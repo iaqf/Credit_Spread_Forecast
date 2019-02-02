c00855e1daf55e800b41eb63734c49174807a1e3
SkipToMyLou <lou-key@163.com>
2019-02-02 10:06:23 -0600

    - create folders

---
+++ src/CSF.py
@@ -0,0 +1,6 @@
+__author__ = 'Yijun Lou, Changjie Ma, Xiaokang Feng, Ning Fan, Xiaoman Gong, Ziheng Zhou'
+__copyright__ = "Copyright 2019, The Group Project of IAQF"
+__credits__ = ["Yijun Lou", "Changjie Ma", "Xiaokang Feng", "Ning Fan", "Xiaoman Gong", "Ziheng Zhou"]
+__license__ = "University of Illinois, Urbana Champaign"
+__version__ = "1.0.0"
+
